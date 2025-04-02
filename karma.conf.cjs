47169cc5d731f9f1984c01436dc58364bdc99e2b
Albert Bausili <albert.bausili@gmail.com>
2025-04-02 12:34:20 +0200

    feat: added automated testing for v1 and v2. Related to: #1

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,26 +1,31 @@
 module.exports = function(config) {
-    config.set({
-      basePath: '',
-      frameworks: ['mocha', 'chai'],
-      files: [
-        'node_modules/htmx.org/dist/htmx.js',
-        'src/dynamic-url.js',
-        'test/test.js'
-      ],
-      exclude: [],
-      preprocessors: {},
-      reporters: ['progress'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: false,
-      browsers: ['ChromeHeadless'],
-      singleRun: true,
-      concurrency: Infinity,
-      client: {
-         mocha: {
-           ui: 'bdd',
-         }
-      }
-    })
-  }
+  const htmxVersion = process.env.HTMX_VERSION || 'v1';
+  const htmxPath = `node_modules/htmx-${htmxVersion}/dist/htmx.js`;
+
+  console.log(`[Karma Config] Loading HTMX version '${htmxVersion}' from: ${htmxPath}`);
+
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      htmxPath,
+      'src/dynamic-url.js',
+      'test/test.js'
+    ],
+    exclude: [],
+    preprocessors: {},
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    concurrency: Infinity,
+    client: {
+       mocha: {
+         ui: 'bdd',
+       }
+    }
+  })
+}
